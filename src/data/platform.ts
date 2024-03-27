5ff3d781aba9bfda8510b3d31ab46202c4040708
yutou-air <xinyu198736@gmail.com>
2024-03-27 12:17:59 +0800

    Add baichuan ai

--- src/data/platform.ts
+++ src/data/platform.ts
@@ -1,18 +1,19 @@
 import {price_openai} from "@/data/openai";
 import {price_azure} from "@/data/azure";
 import {price_moonshot} from "@/data/moonshot";
-import { price_claude } from '@/data/claude';
-import { price_zhipu } from '@/data/zhipu';
+import {price_claude} from '@/data/claude';
+import {price_zhipu} from '@/data/zhipu';
+import {price_baichuan} from "@/data/baichuan";
 
 export type ModelInterface = {
     model: string;
-    description?:string;
+    description?: string;
     price: {
         input: number;
         output?: number;
         training?: number;
     };
-    tags: string[];
+    tags: ('common' | 'embedding' | 'fine-tuned' | 'npc')[];
 }
 export type PlatformInterface = {
     name: string;
@@ -21,11 +22,12 @@ export type PlatformInterface = {
     url: string;
     models: ModelInterface[];
 }
-export const platforms: Record<'openai' | 'azure' | 'moonshot'|'claude'|'zhipu', PlatformInterface> = {
+export const platforms: Record<'openai' | 'azure' | 'moonshot' | 'claude' | 'zhipu' | 'baichuan', PlatformInterface> = {
     openai: price_openai,
     azure: price_azure,
     claude: price_claude,
     moonshot: price_moonshot,
-    zhipu: price_zhipu
+    zhipu: price_zhipu,
+    baichuan: price_baichuan
 }
 export const platform_keys = Object.keys(platforms) as Array<keyof typeof platforms>;
